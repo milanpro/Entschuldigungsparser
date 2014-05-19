f31ecd80c69af0ee9b8fe62b540ec703af19710f
Proell.Mil <proell.mil@ltsp.(none)>
2014-05-19 12:50:09 +0200

    erste Version emailparser

---
+++ emailparser.py
@@ -0,0 +1,11 @@
+def parse_mail(text):
+    mailtext = text.split("::")
+    i, name = mailtext[0].split("_",1)
+    i, vorname
+    print mailtext
+    print name
+
+
+parse_mail("name_Proell\n vorname_Milan::Grund_Kein Bock auf Schule")
+
+    
